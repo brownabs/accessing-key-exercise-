82d63572652604cbab0c2f24c398f31060a66856
Abbey Brown <abbeybrownsemail@gmail.com>
2019-01-17 09:03:16 -0600

    accessing-key-exercise

---
+++ main.js
@@ -0,0 +1,22 @@
+// Use dot notation to access the value of the key "meaning_of_life" in this object
+
+let hitchhikers_guide = {
+    characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "Don't Panic",
+    random_facts: {
+        copies_sold: 14000000,
+        formats: ["radio", "TV", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life: 42
+        }
+    }
+};
+
+console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
+
+
+// Now access it using a variable called "meaning": let meaning = "meaning_of_life"
+
+const meaning = "meaning_of_life"
+let meaningOfLife = hitchhikers_guide.random_facts.ultimate_answer[meaning]
+console.log(meaningOfLife)
